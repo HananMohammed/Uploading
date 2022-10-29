a76388c4e428dcbcf450bae8399aff3b3c44241b
HananMohammed <hananmohammed2468@gmail.com>
2022-10-29 13:33:49 +0200

    Saving Photo Data

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
     public function index()
     {
-        return  view('image.index');
+        return view('image.index');
     }
 
     public function show()
@@ -18,6 +19,27 @@ class ImageController extends Controller
 
     public function store(Request $request)
     {
-        //save images
+        if (!$request->has('image'))
+            return response()->json(['error' => 'There is no image Uploaded ..']);
+
+        $request->validate([
+            'image' => 'required|file|image'
+        ]);
+
+        $path = $request->file('image')->store('public/images');
+
+        if (!$path) {
+            return response()->json(['error', 'The File Cant be Saved'], 500);
+        }
+
+        $uploadedFile = $request->file('image');
+
+        $image = Image::create([
+            'name' => $uploadedFile->hashName(),
+            'extension' => $uploadedFile->extension(),
+            'size' => $uploadedFile->getSize(),
+        ]);
+
+        return $image;
     }
 }
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -4,7 +4,6 @@
           name="image"
           ref="pond"
           label-idle="Click to Choose image, or Drag Here..."
-          server="/upload"
           @init="handleFilePondInit"
           accepted-file-types="image/*"
       />
@@ -13,7 +12,7 @@
 </template>
 
 <script>
-import vueFilePond from 'vue-filepond';
+import vueFilePond, {setOptions} from 'vue-filepond';
 // Import plugins
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.esm.js';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.esm.js';
@@ -21,6 +20,17 @@ import FilePondPluginImagePreview from 'filepond-plugin-image-preview/dist/filep
 // Import styles
 import 'filepond/dist/filepond.min.css';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+
+setOptions({
+    server:{
+        process:{
+            url: '/upload',
+            headers:{
+                'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf_token"]').content
+            }
+        }
+    }
+});
 
 const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
 
