ebfe0d26c079107ab362a7be424fc7b2f3cabffa
HananMohammed <hananmohammed2468@gmail.com>
2022-10-29 13:05:47 +0200

    file pond setup

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,38 @@
 <template>
-    <h2 class="text-xl text-center">{{ message }}</h2>
+  <div class="mt-3">
+      <file-pond
+          name="image"
+          ref="pond"
+          label-idle="Click to Choose image, or Drag Here..."
+          server="/upload"
+          @init="handleFilePondInit"
+          accepted-file-types="image/*"
+      />
+  </div>
+
 </template>
 
 <script>
+import vueFilePond from 'vue-filepond';
+// Import plugins
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.esm.js';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.esm.js';
+
+// Import styles
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
+
 export default {
-    data(){
-        return{
-            message: "Hello World"
+    components: {
+        FilePond,
+    },
+    methods:{
+        handleFilePondInit()
+        {
+            console.log('Filepond Initialized')
+            console.log('Filepond Object :', this.$refs.pond)
         }
     }
 }
